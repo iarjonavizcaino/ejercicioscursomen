8f6e3800417a1f3e27f9c9e657f4e764cc8b1752
desarrollo-001 <desarrollo-001@iMac-de-desarrollo-001.local>
2017-06-14 13:42:30 -0600

    Ejercicio 009

---
+++ 009_exercisemg/index.js
@@ -0,0 +1,5 @@
+var converter = require ('number-to-words');
+
+console.log(converter.toWords(9007199254740992));
+console.log(converter.toOrdinal(9007199254740992));
+console.log(converter.toWordsOrdinal(9007199254740992));
